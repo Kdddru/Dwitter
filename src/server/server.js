1233e7a879f9ad4cfc8df498f4390ccc03a4324e
Kdddru <125441996+Kdddru@users.noreply.github.com>
2024-01-11 18:07:43 +0900

    .env추가 및 키변경

--- src/server/server.js
+++ src/server/server.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+//import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -8,17 +8,17 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyC3dLb5c-kFCNvtOexZTObWkFwQOfKR9IA",
-  authDomain: "dwitter-c2a57.firebaseapp.com",
-  projectId: "dwitter-c2a57",
-  storageBucket: "dwitter-c2a57.appspot.com",
-  messagingSenderId: "105451401577",
-  appId: "1:105451401577:web:d2c0ce1396aafb6cb98df8",
-  measurementId: "G-6FPEJG3PZ0"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+//const analytics = getAnalytics(app);
 
 export const auth = getAuth(app);
